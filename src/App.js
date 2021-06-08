e59cad7d59655767eee478804ed97cd8b50e057e
unknown <kun_duc123@yahoo.com.vn>
2021-06-08 22:23:55 +0700

    Add Profile Page

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Game from './pages/Game/Game.jsx';
 import Home from './pages/Home/Home.jsx';
+import Profile from './pages/Profile/Profile.jsx';
 import NotFound from './pages/NotFound/NotFound.jsx';
+
+
 import './App.css';
 
 function App() {
@@ -11,6 +14,7 @@ function App() {
       <Switch>
         <Route exact path="/"component={Home}/>
         <Route exact path="/play" component={Game} />
+        <Route exact path="/profile" component={Profile}/>
         <Route component={NotFound}></Route>
       </Switch>
     </BrowserRouter>
